1c07077ec1647fd481e7b541d4e9dfc048c7ef6f
Serge Fedorov <Lir2ooo5@yandex.ru>
2024-01-29 22:58:01 +0300

    feat: add answer read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import {promises as fs} from 'fs';
 const read = async () => {
     // Write your code here 
+    const filePath = './files/fileToRead.txt';
+
+    try {
+        await fs.access(filePath);
+        const content = await fs.readFile(filePath, 'utf-8');
+        console.log(content);
+    } catch(err){
+        if(err.code = 'ENOENT') {
+            throw new Error('FS ERROR');
+        }
+        throw err;
+    }
 };
 
 await read();
