545d3a263d1f5b474f03b112c73e153802c9517f
Serge Fedorov <Lir2ooo5@yandex.ru>
2024-01-29 21:11:53 +0300

    feat: add answer for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import {promises as fs} from 'fs'
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.writeFile('./files/fresh.txt', 'I am fresh and young', { flag: 'wx' });
+    } catch (err) {
+        if (err.code === 'EEXIST') {
+            throw new Error('Error! such a file already exists');
+        }
+        throw err;
+    }
 };
 
 await create();
