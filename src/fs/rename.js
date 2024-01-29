0553b1afa9fcb47f3977d25a415d129037d9c3e5
Serge Fedorov <Lir2ooo5@yandex.ru>
2024-01-29 22:38:05 +0300

    feat: add answer rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import {promises as fs} from 'fs';
+import path from 'path';
+
 const rename = async () => {
     // Write your code here 
+    const oldPath = './files/wrongFilename.txt';
+    const newPath = './files/properFilename.md';
+
+    try {
+        await fs.access(oldPath);
+        await fs.access(newPath).then(() => {throw new Error('FS ERORR');}).catch((err) => { if(err.code !== 'ENOENT') throw err;});
+        await fs.rename(oldPath, newPath);
+    } catch (err){
+        throw err;
+    }
+
 };
 
 await rename();
