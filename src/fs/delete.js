819cdf4870c84abe3b51ec1210f59c84d2c12e6c
Serge Fedorov <Lir2ooo5@yandex.ru>
2024-01-29 22:45:21 +0300

    feat: add answer delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import {promises as fs} from 'fs';
+
 const remove = async () => {
     // Write your code here 
+    const filePath = './files/fileToRemove.txt';
+
+    try {
+        await fs.access(filePath);
+        await fs.unlink(filePath);
+    }catch (err){
+        if(err.code ==='ENOENT'){
+            throw new Error('FS ERROR')
+        }
+        throw err;
+    }
 };
 
 await remove();
